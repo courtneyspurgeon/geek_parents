c6a1700c5222360661ce5cd78b2955a08948d47d
courtney spurgeon <spurgeon.cj@gmail.com>
2013-07-09 10:37:13 -0700

    restoring newsplus footer.php to original, making changes in newsplus_custom instead (go wordpress)

--- wp-content/themes/newsplus/footer.php
+++ wp-content/themes/newsplus/footer.php
@@ -13,30 +13,48 @@ global $pls_hide_secondary, $pls_footer_left, $pls_footer_right, $post; ?>
 /* Check if the secondary widget areas are active */
 if ( is_active_sidebar( 'secondary-column-1' ) || is_active_sidebar( 'secondary-column-2' ) || is_active_sidebar( 'secondary-column-3' ) || is_active_sidebar( 'secondary-column-4' ) ) :
 
-	/* Check if the user has opted to hide widget area */
-	if ( is_page() ) {
-		$page_opts = get_post_meta( $posts[0]->ID, 'page_options', true );
-		$hide_secondary = isset( $page_opts[ 'hide_secondary' ] ) ? $page_opts[ 'hide_secondary' ] : '';
-	} // is page
-	elseif ( is_single() ) {
-		$post_opts = get_post_meta( $posts[0]->ID, 'post_options', true );
-		$hide_secondary = isset( $post_opts[ 'hide_secondary' ] ) ? $post_opts[ 'hide_secondary' ] : '';
-	} // is single
-	else {
-		$hide_secondary = $pls_hide_secondary;
-	}
-	if ( 'true' != $hide_secondary ) : ?>
+    /* Check if the user has opted to hide widget area */
+    if ( is_page() ) {
+        $page_opts = get_post_meta( $posts[0]->ID, 'page_options', true );
+        $hide_secondary = isset( $page_opts[ 'hide_secondary' ] ) ? $page_opts[ 'hide_secondary' ] : '';
+    } // is page
+    elseif ( is_single() ) {
+        $post_opts = get_post_meta( $posts[0]->ID, 'post_options', true );
+        $hide_secondary = isset( $post_opts[ 'hide_secondary' ] ) ? $post_opts[ 'hide_secondary' ] : '';
+    } // is single
+    else {
+        $hide_secondary = $pls_hide_secondary;
+    }
+    if ( 'true' != $hide_secondary ) : ?>
         <div id="secondary" role="complementary">
             <div class="wrap clear">
-                <div class="column full-width">
-					<?php
+                <div class="column one-fourth">
+                    <?php
                     if ( is_active_sidebar( 'secondary-column-1' ) )
-						dynamic_sidebar( 'secondary-column-1' );
+                        dynamic_sidebar( 'secondary-column-1' );
                     ?>
                 </div><!-- .column one-fourth -->
+                <div class="column one-fourth">
+                    <?php
+                    if ( is_active_sidebar( 'secondary-column-2' ) )
+                        dynamic_sidebar( 'secondary-column-2' );
+                    ?>
+                </div><!-- .column one-fourth -->
+                <div class="column one-fourth">
+                    <?php
+                    if ( is_active_sidebar( 'secondary-column-3' ) )
+                        dynamic_sidebar( 'secondary-column-3' );
+                    ?>
+                </div><!-- .column one-fourth -->
+                <div class="column one-fourth last">
+                    <?php
+                    if ( is_active_sidebar( 'secondary-column-4' ) )
+                        dynamic_sidebar( 'secondary-column-4' );
+                    ?>
+                </div><!-- .column one-fourth .last -->
             </div><!-- #secondary .wrap -->
         </div><!-- #secondary -->
-	<?php endif; // hide secondary
+    <?php endif; // hide secondary
 endif; // if widget areas are active ?>
 <footer id="footer" role="contentinfo">
     <div class="wrap clear">
